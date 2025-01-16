32c6294098aa13a7c92d9facf37da130effc70b3
MwangangiBrian <bmwangimwangangi@gmail.com>
2025-01-16 18:07:59 +0300

    fix: imagekit public key

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ export const config = {
   env: {
     imagekit: {
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-      publicKey: process.env.EXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
     },
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
